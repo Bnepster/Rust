fce05bec48c0b621fcdbf907a0f7af93406ce78e
bruno-silva97 <124637369+bruno-silva97@users.noreply.github.com>
2023-10-23 18:37:01 -0300

    atualização 23/10

---
+++ ex_doc_rust/capitulo03/Funções/target/debug/deps/Funções.d
@@ -0,0 +1,5 @@
+C:\Users\bruno\Documents\Bnepster97\Rust\ex_doc_rust\capitulo03\Funções\target\debug\deps\Funções.exe: src\main.rs
+
+C:\Users\bruno\Documents\Bnepster97\Rust\ex_doc_rust\capitulo03\Funções\target\debug\deps\Funções.d: src\main.rs
+
+src\main.rs:
