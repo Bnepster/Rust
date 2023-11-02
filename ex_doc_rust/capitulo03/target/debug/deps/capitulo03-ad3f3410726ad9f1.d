99876b3c3ab1480b8f4159ed062ce4b2f913e1f0
Bruno de Sousa <103311338+Bnepster@users.noreply.github.com>
2023-11-02 17:40:32 -0300

    atualização 02/11

---
+++ ex_doc_rust/capitulo03/target/debug/deps/capitulo03-ad3f3410726ad9f1.d
@@ -0,0 +1,5 @@
+C:\Users\bsouz\OneDrive\Documentos\GitHub\Rust\ex_doc_rust\capitulo03\target\debug\deps\capitulo03-ad3f3410726ad9f1.rmeta: src\main.rs
+
+C:\Users\bsouz\OneDrive\Documentos\GitHub\Rust\ex_doc_rust\capitulo03\target\debug\deps\capitulo03-ad3f3410726ad9f1.d: src\main.rs
+
+src\main.rs:
---
+++ ex_doc_rust/capitulo03/target/debug/deps/capitulo03-ea79fc17fda8d827.d
@@ -0,0 +1,5 @@
+C:\Users\bsouz\OneDrive\Documentos\GitHub\Rust\ex_doc_rust\capitulo03\target\debug\deps\capitulo03-ea79fc17fda8d827.rmeta: src\main.rs
+
+C:\Users\bsouz\OneDrive\Documentos\GitHub\Rust\ex_doc_rust\capitulo03\target\debug\deps\capitulo03-ea79fc17fda8d827.d: src\main.rs
+
+src\main.rs:
