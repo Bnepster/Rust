5bfcfeb0a090eaee45505d153613711f0e836cfd
Bruno de Sousa <103311338+Bnepster@users.noreply.github.com>
2023-11-15 21:03:50 -0300

    atualizar

---
+++ ex_doc_rust/capitulo03/Funções/target/debug/deps/Funções-bc83794ee8cb6458.d
@@ -0,0 +1,5 @@
+C:\Users\bsouz\OneDrive\Documentos\GitHub\Rust\ex_doc_rust\capitulo03\Funções\target\debug\deps\Funções-bc83794ee8cb6458.rmeta: src\main.rs
+
+C:\Users\bsouz\OneDrive\Documentos\GitHub\Rust\ex_doc_rust\capitulo03\Funções\target\debug\deps\Funções-bc83794ee8cb6458.d: src\main.rs
+
+src\main.rs:
---
+++ ex_doc_rust/capitulo03/Funções/target/debug/deps/Funções-fb6b0f62226dffe2.d
@@ -0,0 +1,5 @@
+C:\Users\bsouz\OneDrive\Documentos\GitHub\Rust\ex_doc_rust\capitulo03\Funções\target\debug\deps\Funções-fb6b0f62226dffe2.rmeta: src\main.rs
+
+C:\Users\bsouz\OneDrive\Documentos\GitHub\Rust\ex_doc_rust\capitulo03\Funções\target\debug\deps\Funções-fb6b0f62226dffe2.d: src\main.rs
+
+src\main.rs:
