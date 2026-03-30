6906a950d7eb5d099b38c51e8686e2bf78b7ec67
Bruno de Sousa <124637369+bruno-silva97@users.noreply.github.com>
2026-03-30 15:00:11 -0300

    Atualização

---
+++ ex_doc_rust/capitulo03/Comentarios/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+   // Esse é um comentário de apenas uma linha  
+   
+   // Esse cpmentario é de '\n
+   // multiplas linhas '
+
+   let exemplo = "Exemplo de variavel" // Esse é um exemplo de como podemos escrever um comentário ao final de uma linha de código.
+
+   // Normalmente um comentário é feito acima da linha de código, tipo assim.
+   let codigo_de_exemplo = "Novo exemplo de código"
+
+   // Chegamos ao final desse tópico com esse breve comentátio.
+
+}
---
+++ ex_doc_rust/capitulo03/Fluxo_de_controle/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    //expressão if
+    let numero = 3;
+
+    if < 5 {
+        println!("Condição verdadeira");
+    } ekse {
+        println!("Condição falsa")
+    }
+
+    
+
+}
